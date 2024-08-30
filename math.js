8e38365870d7b037e470dbc52b07a839df136fbf
HP <HP@DESKTOP-8E0FCDH>
2024-08-30 22:41:16 +0600

    variable.js

---
+++ math.js
@@ -0,0 +1,16 @@
+/**
+ * **************************************
+ * MATH NEEDED FOR PROGRAMMING
+ * **************************************
+ * 1.NUMBER:+VE,-VE,0 [50,-9,0]
+ * 2.integer(porno-songkha):2,88,{ODD[bijor],EVEN[jor]}
+ * 3.FLOAT:5.5,6.7
+ * float:+,-,/
+ * more operation:%,{vagses []}
+ * percentage:5%,50 taka er 10% koto???
+ * sum:14,18,16
+ * avg:
+ * max/min:2,564,66 (3 ta songkha dilam.er moddhe sobcey boro konta,,sobceye soto konta)
+ * series(dhara)
+ * area:triangle{khetro ful}
+ */
---
+++ variable.js
@@ -0,0 +1,38 @@
+/**
+ * VARIABLE:
+ *
+ */
+
+// module task practise
+
+var money = 345;
+var age = 23;
+console.log(money);
+console.log(age);
+var number = 5;
+var weight = 55;
+var time = 3.45;
+console.log(weight);
+console.log(time);
+console.log(number);
+
+var price = 5005;
+price = 2003;
+console.log(price);
+
+weight = 23;
+time = 2;
+
+//  * new javascript practise task
+
+var salary = 500000;
+salary = 2000;
+console.log(salary);
+
+var height = 5;
+height = 2;
+console.log(height);
+
+var demand = 45;
+demand = 34;
+console.log(demand);
