2d7700150bce8a527207da88e4c5c09400129e19
HP <HP@DESKTOP-8E0FCDH>
2024-08-31 23:44:18 +0600

    mixedOp.js

--- mixedOP.js
+++ mixedOP.js
@@ -1,3 +1,25 @@
-var first = "mobarak";
-var second = "mobarak";
-var fullName = first + second;
+// var first = "mobarak";
+// var second = "mobarak";
+// var fullName = first + second;
+// console.log(fullName);
+
+// var first = "shila";
+// var third = "kila";
+// var full = first + third;
+// console.log(full);
+
+// console.log(5 + "40");
+// // jog er khetre manena string ta badh baki sob mane!
+// console.log(5 - "40");
+// console.log(5 * "40");
+// console.log(5 / "40");
+
+var fame = "jila";
+var name = "hila";
+var onlyName = fame + name;
+console.log(onlyName);
+
+console.log("34" + 4);
+console.log("34" * 4);
+console.log("34" / 4);
+console.log("34" - 4);
